80344710c15fe430856fa71a0a0af17ec355e4bd
sandada035-blip <sandada035@gmail.com>
2026-03-04 07:08:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "attendance-summary-pwa-v1";
+const CACHE = "attendance-summary-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -33,3 +33,4 @@ self.addEventListener("fetch", (event) => {
   // Static: cache-first
   event.respondWith(caches.match(event.request).then((c)=>c || fetch(event.request)));
 });
+
